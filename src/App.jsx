311b94303a3454a2b3919f83f0d61a2b1f5dc767
shinwokkang <120699742+shinwokkang@users.noreply.github.com>
2025-02-02 21:22:39 +0900

    day10 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import OrderEditor from "./components/OrderEditor";
 
+const Welcome = () => {
+  const name = "shinWook";
+  const isMember = true;
+
+  return (
+    <h1>
+      {isMember
+        ? `${name}님 다시 오셨군요`
+        : `${name}님
+      가입하시겠어요?`}
+    </h1>
+  );
+};
+
 function App() {
   return (
     <>
-      <OrderEditor />
+      <Welcome />
     </>
   );
 }
